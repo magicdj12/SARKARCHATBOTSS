c6e5b03adcfc8b5a006e3a49876430a97738cf00
Magichidden <gf502429@gmail.com>
2025-01-27 12:57:54 +0330

    Update userbot.py

--- nexichat/userbot/userbot.py
+++ nexichat/userbot/userbot.py
@@ -8,41 +8,43 @@ import config
 
 class Userbot(Client):
     def __init__(self):
+        # ایجاد یک نمونه از کلاینت تلگرام
         self.one = Client(
-            name="VIPAss1",
-            api_id=config.API_ID,
-            api_hash=config.API_HASH,
-            session_string=str(config.STRING1),
-            no_updates=False,
-            plugins=dict(root="nexichat.idchatbot"),
+            name="VIPAss1",  # نام ربات
+            api_id=config.API_ID,  # شناسه API تلگرام
+            api_hash=config.API_HASH,  # هش API تلگرام 
+            session_string=str(config.STRING1),  # رشته نشست
+            no_updates=False,  # دریافت آپدیت‌ها فعال است
+            plugins=dict(root="nexichat.idchatbot"),  # مسیر پلاگین‌ها
         )
         
 
     async def start(self):
-        print(f"Starting Id chatbot...")
+        print("در حال راه‌اندازی ربات چت آیدی...")
 
-        if config.STRING1:
-            await self.one.start()
+        if config.STRING1:  # اگر رشته نشست موجود باشد
+            await self.one.start()  # شروع کلاینت
             try:
+                # عضویت در کانال‌های تلگرام
                 await self.one.join_chat("TG_NAME_STYLE")
-                await self.one.join_chat("TG_NAME_STYLE")
+                await self.one.join_chat("TG_NAME_STYLE") 
                 await self.one.join_chat("TG_NAME_STYLE")
                 await self.one.join_chat("TG_NAME_STYLE")
 
             except:
                 pass
-            self.one.id = self.one.me.id
-            self.one.name = self.one.me.mention
-            self.one.username = self.one.me.username
-     
-            print(f"Id-Chatbot Started as {self.one.me.first_name}")
             
-        
-
+            # ذخیره اطلاعات کاربر
+            self.one.id = self.one.me.id  # شناسه کاربر
+            self.one.name = self.one.me.mention  # نام کاربر 
+            self.one.username = self.one.me.username  # نام کاربری
+     
+            print(f"ربات چت آیدی با نام {self.one.me.first_name} شروع به کار کرد")
+            
     async def stop(self):
-        LOGGER(__name__).info(f"Stopping Id-Chatbot...")
+        LOGGER(__name__).info("در حال متوقف کردن ربات چت آیدی...")
         try:
             if config.STRING1:
-                await self.one.stop()
+                await self.one.stop()  # توقف کلاینت
         except:
             pass
