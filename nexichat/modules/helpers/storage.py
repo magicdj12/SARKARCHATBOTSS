c822bb7652462588244c4c2771c06dfc58dd3575
magicdj12 <moha.fake@gmail.com>
2025-01-28 19:16:32 +0330

    Update storage.py

--- nexichat/modules/helpers/storage.py
+++ nexichat/modules/helpers/storage.py
@@ -1,11 +1,13 @@
 import random
 from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 
-# لینک اتصال به پایگاه داده مونگو
-MONGO_URL = "mongodb+srv://gpsfardi:mohaMmoha900@cluster0.fj1u6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+CHAT_STORAGE = [
+    "mongodb+srv://m33537924:JLBRhGx8FLxy43c@cluster0.zy8ld.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    "mongodb+srv://Yash_607:Yash_607@cluster0.r3s9sbo.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://gpsfardi:mohaMmoha900@cluster0.fj1u6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+]
 
-# ایجاد اتصال به پایگاه داده
-VIPBOY = MongoCli(MONGO_URL)
+VIPBOY = MongoCli(random.choice(CHAT_STORAGE))
 chatdb = VIPBOY.Anonymous
 chatai = chatdb.Word.WordDb
 storeai = VIPBOY.Anonymous.Word.NewWordDb
