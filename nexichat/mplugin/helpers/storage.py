8085ab32d3eb0cb4922b23bec6f5651606403d89
magicdj12 <moha.fake@gmail.com>
2025-02-04 16:47:24 +0330

    Update storage.py

--- nexichat/mplugin/helpers/storage.py
+++ nexichat/mplugin/helpers/storage.py
@@ -3,7 +3,7 @@ from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 
 CHAT_STORAGE = [
     "mongodb+srv://m33537924:JLBRhGx8FLxy43c@cluster0.zy8ld.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-    "mongodb+srv://Yash_607:Yash_607@cluster0.r3s9sbo.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://ranger:mohaMmoha900@cluster2.24a45.mongodb.net/?retryWrites=true&w=majority&appName=Cluster2",
     "mongodb+srv://gpsfardi:mohaMmoha900@cluster0.fj1u6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 ]
 
