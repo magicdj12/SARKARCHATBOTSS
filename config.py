1aaed458e5d63f78d258de64a259099e84bb1ab9
magicdj12 <moha.fake@gmail.com>
2025-02-04 16:23:51 +0330

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "22543203"
+API_ID = "27573845"
 # -------------------------------------------------------------
-API_HASH = "6b0baaf3002dd71f048cba92774b0c55"
+API_HASH = "2785648d5964c7d734f561169c773335"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 STRING1 = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "7154410907"))
-SUPPORT_GRP = "Poshtibaninetroplusbot"
-UPDATE_CHNL = "me_nitroplus"
+OWNER_ID = int(getenv("OWNER_ID", "5747542213"))
+SUPPORT_GRP = "+MS2tY8FN4eExZTA8"
+UPDATE_CHNL = "music_rock22"
 OWNER_USERNAME = "Owner_nitroplus"
