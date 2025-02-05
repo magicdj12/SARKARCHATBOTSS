ad9c825b52296de93648642b506113301819788b
magicdj12 <moha.fake@gmail.com>
2025-02-05 07:14:19 +0330

    Update storage.py

--- nexichat/modules/helpers/storage.py
+++ nexichat/modules/helpers/storage.py
@@ -2,9 +2,8 @@ import random
 from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 
 CHAT_STORAGE = [
-    "mongodb+srv://m33537924:JLBRhGx8FLxy43c@cluster0.zy8ld.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
     "mongodb+srv://ranger:mohaMmoha900@cluster2.24a45.mongodb.net/?retryWrites=true&w=majority&appName=Cluster2",
-    "mongodb+srv://gpsfardi:mohaMmoha900@cluster0.fj1u6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    
 ]
 
 VIPBOY = MongoCli(random.choice(CHAT_STORAGE))
